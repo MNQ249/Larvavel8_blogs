b25b6fbf25dc3be396ab55069cb5a29e3024ce4a
mesfer nasser <mesfer.vip@gmail.com>
2022-01-10 12:57:35 +0300

    first--how a route load view + include css and javascript + make a Route and Link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="test.css">
+        <script src="test.js"></script>
+        <title>My Blog</title>
+
+        <!-- Fonts -->
+
+
+        <!-- Styles -->
+
+    </head>
+    <body>
+            <h1>You and I</h1>
+
+          <article>
+              <h2><a href="/post">My First Post</a></h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolorem eius hic in itaque officiis quod rem repellendus? Amet cumque dolores enim illo incidunt iure maxime officiis omnis praesentium sit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate distinctio dolor dolores eaque fugiat, iusto! A ad, corporis dignissimos doloribus eaque error fugit molestiae nobis perferendis quaerat quidem repellat sequi!</p>
+          </article>
+
+
+            <article>
+                <h2><a href="/post">My Second Post</a></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolorem eius hic in itaque officiis quod rem repellendus? Amet cumque dolores enim illo incidunt iure maxime officiis omnis praesentium sit.</p>
+            </article>
+
+
+            <article>
+                <h2><a href="/th_post">My Third Post</a></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolorem eius hic in itaque officiis quod rem repellendus? Amet cumque dolores enim illo incidunt iure maxime officiis omnis praesentium sit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet, consequatur cum cumque delectus dicta dolores dolorum esse eveniet explicabo maxime nihil officiis possimus provident qui sapiente sed temporibus tenetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores autem blanditiis, cum dignissimos distinctio enim ex explicabo fugiat ipsa iure nesciunt nostrum, odit qui quisquam ratione tempora vero, voluptas?</p>
+            </article>
+
+
+
+    </body>
+
+</html>
